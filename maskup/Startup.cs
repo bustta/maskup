fbad3eeb5e9b99002336e16e98b6bce6f5cead93
bustta <bustta80980@gmail.com>
2015-07-10 10:40:42 +0800

    init project

---
+++ maskup/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(maskup.Startup))]
+namespace maskup
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
